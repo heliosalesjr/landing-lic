8f92aa783088fdb6e5a0d791b67e1945091f42c9
heliosalesjr <heliosalesjr@gmail.com>
2024-07-10 18:27:47 -0300

    requested updates from the board

--- app/components/AccordionContent.jsx
+++ app/components/AccordionContent.jsx
@@ -22,15 +22,8 @@ export default function Acc() {
             <AccordionItem key="2" aria-label="Caderno do Estudante" subtitle="Clique para abrir" title="Caderno do Estudante" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
                 <iframe src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=cnio385hp8" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
             </AccordionItem>
-            <AccordionItem key="3" aria-label="Accordion 3" subtitle="Clique para abrir" title="Avaliações digitais" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
-                <iframe src="https://sites.google.com/beieducacaopedagogico.com/aluno6oano?usp=sharing#h.o5i6rkw6s4zi" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
-            </AccordionItem>
-            <AccordionItem key="4" aria-label="Accordion 4" subtitle="Clique para abrir" title="Aprendizagem Baseada em Projetos" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
-                
-            </AccordionItem>
-            <AccordionItem key="5" aria-label="Accordion 5" subtitle="Clique para abrir" title="Ferramentas Participativas" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
-                
-            </AccordionItem>
+            
+            
         </Accordion>
     </div>
     </div>
