dcbdb8e91c3c8689482425860c63389ebc6321f5
heliosalesjr <heliosalesjr@gmail.com>
2024-07-13 22:31:03 -0300

    renaming dropdown items

--- app/components/AccordionContent.jsx
+++ app/components/AccordionContent.jsx
@@ -16,10 +16,10 @@ export default function Acc() {
 
         <div className='max-w-5xl mx-auto'>
         <Accordion defaultExpandedKeys={["1"]}>
-            <AccordionItem key="1" aria-label="Livro do Professor" subtitle="Clique para abrir" title="Livro do Professor" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
+            <AccordionItem key="1" aria-label="Vídeo Tutorial" subtitle="Clique para abrir" title="Vídeo Tutorial" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
                 <iframe src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=7u5rif8unf" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
             </AccordionItem>
-            <AccordionItem key="2" aria-label="Caderno do Estudante" subtitle="Clique para abrir" title="Caderno do Estudante" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
+            <AccordionItem key="2" aria-label="Caderno do Estudante" subtitle="Clique para abrir" title="Manual de Acesso à Plataforma" className="hover:bg-sky-100 transition-colors duration-200 rounded-lg px-4 font-bold dark:hover:bg-sky-700">
                 <iframe src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=cnio385hp8" width="100%" height="600" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen allow="autoplay; clipboard-read; clipboard-write"></iframe>
             </AccordionItem>
             
